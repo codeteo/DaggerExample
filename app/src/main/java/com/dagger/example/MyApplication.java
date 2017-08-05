d26e9f6cfe4d9223fd09c0f4d67e1b3053038c9d
teo <ramones.teo@gmail.com>
2017-08-05 13:31:39 +0300

    Exposes ApplicationComponent from MyApplication

--- app/src/main/java/com/dagger/example/MyApplication.java
+++ app/src/main/java/com/dagger/example/MyApplication.java
@@ -3,11 +3,18 @@ package com.dagger.example;
 import android.app.Activity;
 import android.app.Application;
 
+import com.dagger.example.dagger.ApplicationComponent;
+import com.dagger.example.dagger.ApplicationModule;
+import com.dagger.example.dagger.DaggerApplicationComponent;
+import com.dagger.example.dagger.DataModule;
+
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
+import timber.log.Timber;
+import timber.log.Timber.DebugTree;
 
 /**
  * Application class for the app.
@@ -18,9 +25,22 @@ public class MyApplication extends Application implements HasActivityInjector {
     @Inject
     DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
 
+    private static ApplicationComponent applicationComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new DebugTree());
+        }
+
+        applicationComponent = DaggerApplicationComponent
+                .builder()
+                .applicationModule(new ApplicationModule(this))
+                .dataModule(new DataModule())
+                .build();
+
+        applicationComponent.inject(this);
 
     }
 
@@ -29,4 +49,7 @@ public class MyApplication extends Application implements HasActivityInjector {
         return dispatchingAndroidInjector;
     }
 
+    public static ApplicationComponent getApplicationComponent() {
+        return applicationComponent;
+    }
 }
