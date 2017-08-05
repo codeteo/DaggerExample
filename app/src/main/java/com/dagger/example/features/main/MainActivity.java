53b3f1355ff9259fed1243666f1cee0560fb16a8
teo <ramones.teo@gmail.com>
2017-08-05 13:53:27 +0300

    Adds MainFragment for main screen

--- app/src/main/java/com/dagger/example/features/main/MainActivity.java
+++ app/src/main/java/com/dagger/example/features/main/MainActivity.java
@@ -1,7 +1,11 @@
-package com.dagger.example;
+package com.dagger.example.features.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dagger.example.R;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fl_container, MainFragment.newInstance())
+                .commit();
+
     }
 }
---
+++ app/src/main/java/com/dagger/example/features/main/MainFragment.java
@@ -0,0 +1,20 @@
+package com.dagger.example.features.main;
+
+import android.os.Bundle;
+
+/**
+ * Displays weather data
+ */
+
+public class MainFragment extends android.support.v4.app.Fragment {
+
+    public static MainFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        MainFragment fragment = new MainFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }    
+    
+}
