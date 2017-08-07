7d052a0e6e0287a572f209f8302f0cf7c259341b
teo <ramones.teo@gmail.com>
2017-08-07 12:28:46 +0300

    Injects presenter to MainFragment

--- app/src/main/java/com/dagger/example/features/main/MainFragment.java
+++ app/src/main/java/com/dagger/example/features/main/MainFragment.java
@@ -1,6 +1,14 @@
 package com.dagger.example.features.main;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dagger.example.MyApplication;
+import com.dagger.example.R;
+import com.dagger.example.features.main.dagger.DaggerMainComponent;
 
 /**
  * Displays weather data
@@ -16,4 +24,14 @@ public class MainFragment extends android.support.v4.app.Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DaggerMainComponent.builder()
+                .applicationComponent(MyApplication.getApplicationComponent())
+                .build()
+                .inject(this);
+    }
+
 }
