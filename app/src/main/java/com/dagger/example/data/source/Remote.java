098f9b8f023d5d89b383f3ba3d800e2e02e2547b
teo <ramones.teo@gmail.com>
2017-08-07 21:35:14 +0300

    Uses Local and Remote interfaces to differentiate dependencies for Dagger

---
+++ app/src/main/java/com/dagger/example/data/source/Remote.java
@@ -0,0 +1,18 @@
+package com.dagger.example.data.source;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+/**
+ *
+ */
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Remote {
+
+}
