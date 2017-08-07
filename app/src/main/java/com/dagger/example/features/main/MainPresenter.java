8cde218ecad8a827a4d78613866634c68fe97f88
teo <ramones.teo@gmail.com>
2017-08-07 12:25:00 +0300

    Uses constructor injection for MainPresenter

--- app/src/main/java/com/dagger/example/features/main/MainPresenter.java
+++ app/src/main/java/com/dagger/example/features/main/MainPresenter.java
@@ -1,10 +1,23 @@
 package com.dagger.example.features.main;
 
+import com.dagger.example.data.rest.UnsplashService;
+
+import javax.inject.Inject;
+
 /**
  * Presenter class for {@link MainFragment}
  */
 
 public class MainPresenter implements MainMVP.Presenter {
+
+    private MainMVP.View view;
+    private UnsplashService unsplashService;
+
+    @Inject
+    public MainPresenter(MainMVP.View view, UnsplashService unsplashService) {
+        this.view = view;
+        this.unsplashService = unsplashService;
+    }
 
     @Override
     public void getPhotos() {
--- app/src/main/java/com/dagger/example/features/main/dagger/MainPresenterModule.java
+++ app/src/main/java/com/dagger/example/features/main/dagger/MainPresenterModule.java
@@ -1,6 +1,8 @@
 package com.dagger.example.features.main.dagger;
 
+import com.dagger.example.data.rest.UnsplashService;
 import com.dagger.example.features.main.MainMVP;
+import com.dagger.example.utils.FragmentScope;
 
 import dagger.Module;
 import dagger.Provides;
@@ -13,13 +15,22 @@ import dagger.Provides;
 public class MainPresenterModule {
 
     private final MainMVP.View view;
+    private final UnsplashService unsplashService;
 
-    public MainPresenterModule(MainMVP.View view) {
+    public MainPresenterModule(MainMVP.View view, UnsplashService unsplashService) {
         this.view = view;
+        this.unsplashService = unsplashService;
     }
 
     @Provides
+    @FragmentScope
     MainMVP.View provideMainView() {
         return view;
+    }
+
+    @Provides
+    @FragmentScope
+    UnsplashService provideUnsplashService() {
+        return unsplashService;
     }
 }
