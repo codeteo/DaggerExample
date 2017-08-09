b3dd83e4da1a88f05b353ed15e5811b8889536be
teo <ramones.teo@gmail.com>
2017-08-09 12:45:34 +0300

    Downloads All images from db

--- app/src/main/java/com/dagger/example/features/main/DownloadPhotosManager.java
+++ app/src/main/java/com/dagger/example/features/main/DownloadPhotosManager.java
@@ -13,6 +13,7 @@ import android.os.Environment;
 import com.dagger.example.data.entities.PhotoDto;
 
 import java.io.File;
+import java.util.Arrays;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -27,13 +28,12 @@ public class DownloadPhotosManager {
 
     private static final String SUFFIX = "jpeg";
 
-    private DownloadManager.Request request;
     private BroadcastReceiver broadcastReceiver;
 
     private Realm realm;
     private Application application;
 
-    private String firstUrl;
+    private RealmResults<PhotoDto> realmUrls;
 
     public DownloadPhotosManager(Realm realm, Application application) {
         this.realm = realm;
@@ -46,52 +46,37 @@ public class DownloadPhotosManager {
     }
 
     private void getUrlsFromDB() {
-        final RealmResults<PhotoDto> realmUrls;
-
         realmUrls = realm.where(PhotoDto.class)
                 .findAll();
-
-        firstUrl = realmUrls.get(0).getDownloadUrl();
     }
 
     private void executeRequest() {
         Timber.i("INSIDE executeRequest");
 
-        Timber.i("downloadURL == %s", firstUrl);
+        final DownloadManager downloadManager = (DownloadManager) application.getSystemService(Context.DOWNLOAD_SERVICE);
 
-        try {
-            request = new DownloadManager.Request(Uri.parse(firstUrl));
-        } catch (IllegalArgumentException e) {
-            Timber.i("Error == %s", e.getMessage());
+        // contains ID for each download request
+        final long[] DL_ID = new long[realmUrls.size()];
+
+        for (int i = 0; i < realmUrls.size(); i++) {
+            DL_ID[i] = downloadManager.enqueue(createRequestForDM(realmUrls.get(i)));
         }
 
-        // allow mobile and WiFi downloads
-        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
-        request.setTitle("DM Example");
-        request.setDescription("Downloading file");
-
-        // set the destination path for this download
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS +
-                File.separator + "image_test", "randomName" + "." + SUFFIX);
-        request.allowScanningByMediaScanner();
-
-        final DownloadManager dm = (DownloadManager) application.getSystemService(Context.DOWNLOAD_SERVICE);
-        // this is our unique download id
-        final long DL_ID = dm.enqueue(request);
-
-        // get notified when the download is complete
+        // get notified when the downloadManager is complete
         broadcastReceiver = new BroadcastReceiver() {
 
             @Override
             public void onReceive(Context context, Intent intent) {
 
-                // our download
-                if (DL_ID == intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1L)) {
+                // our downloadManager
+                long intentID = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1L);
+
+                if (Arrays.asList(DL_ID).contains(intentID)) {
 
                     // get the path of the downloaded file
                     DownloadManager.Query query = new DownloadManager.Query();
-                    query.setFilterById(DL_ID);
-                    Cursor cursor = dm.query(query);
+                    query.setFilterById(intentID);
+                    Cursor cursor = downloadManager.query(query);
                     if (!cursor.moveToFirst()) {
                         Timber.i("Download error: cursor is empty");
                         return;
@@ -104,7 +89,7 @@ public class DownloadPhotosManager {
                     }
 
                     String path = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
-                    Timber.i("File download complete. Location: \n" + path);
+                    Timber.i("File downloadManager complete. Location: \n" + path);
                 }
             }
         };
@@ -112,6 +97,28 @@ public class DownloadPhotosManager {
         // register receiver to listen for ACTION_DOWNLOAD_COMPLETE action
          application.registerReceiver(broadcastReceiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
 
+    }
+
+    private DownloadManager.Request createRequestForDM(PhotoDto photoDto) {
+        DownloadManager.Request request = null;
+
+        try {
+            request = new DownloadManager.Request(Uri.parse(photoDto.getDownloadUrl()));
+        } catch (IllegalArgumentException e) {
+            Timber.i("Error == %s", e.getMessage());
+        }
+
+        // allow mobile and WiFi downloads
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+        request.setTitle("DM Example");
+        request.setDescription("Downloading file");
+
+        // set the destination path for this download
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS +
+                File.separator + "image_test", photoDto.getId() + "." + SUFFIX);
+        request.allowScanningByMediaScanner();
+
+        return request;
     }
 
 }
